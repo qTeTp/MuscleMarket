a87a4632d8175806ea90899c7dbf2ba2ffcc3fcb
Seokwon You <youseokwon1667@gmail.com>
2025-10-30 17:13:41 +0000

    first commit

---
+++ muscle_market/src/test/java/com/example/muscle_market/MuscleMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muscle_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
