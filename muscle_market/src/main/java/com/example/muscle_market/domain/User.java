608f40aa8c6f6c48cb1480cdd9da28d9474a4165
InWoong <dlsdnd122@naver.com>
2025-11-03 13:08:19 +0900

    [FEAT] : UserEntity 생성

---
+++ muscle_market/src/main/java/com/example/muscle_market/domain/User.java
@@ -0,0 +1,51 @@
+package com.example.muscle_market.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String profileImgUrl;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    // 온보딩 상태
+    private Boolean isOnboarded;
+
+    // JPA Lifecycle 콜백
+    // DB 저장, 수정 될 때 자동으로 들어감
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
