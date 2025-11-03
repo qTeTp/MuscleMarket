a915bbbd283cdf3be9318503def15804e59c1dbf
InWoong <dlsdnd122@naver.com>
2025-11-03 13:08:41 +0900

    [FEAT] : SecurityConfig 클래스 생성

---
+++ muscle_market/src/main/java/com/example/muscle_market/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.muscle_market.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()  // h2 콘솔 허용
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .logout(withDefaults());
+
+        // h2 콘솔 관련 설정
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
+
+        return http.build();
+    }
+}
