3890269dad7cfd5d82fc4f8c706cee0069338a67
qTeTp <68058317+qTeTp@users.noreply.github.com>
2025-11-05 17:19:27 +0900

    [FEAT] 거래상태 추가 Transaction 추가 거래를 예약중으로 변경 판매자가 원하는 거래상태로 변경

--- muscle_market/src/main/java/com/example/muscle_market/domain/Transaction.java
+++ muscle_market/src/main/java/com/example/muscle_market/domain/Transaction.java
@@ -1,8 +1,10 @@
 package com.example.muscle_market.domain;
 
+import com.example.muscle_market.domain.enums.TransactionStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
@@ -13,28 +15,55 @@ import java.util.Date;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Transaction {
     @Id
-    @Column(name="transaction_idx")
+    @Column(name = "transaction_idx")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private Integer status; // 거래 상태
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus status;// 거래 상태
 
     @Column
-    private String location;
+    private String location; // 실제 거래를 완료한 거래 장소
 
-    @Column(name = "date")
+    @Column(name = "transaction_date")
     private Date date;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "seller_idx")
-    private User user1;
+    @JoinColumn(name = "seller_idx", nullable = false)
+    private User seller;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "customer_idx")
-    private User user2;
+    @JoinColumn(name = "customer_idx", nullable = false)
+    private User customer;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_post_idx")
     private Product product;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    // 생성 날짜 자동 설정
+    // 업데이트 날짜 자동 설정
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // 거래 상태 변경 메서드
+    public void updateStatus(TransactionStatus newStatus) {
+        // 거래 상태 업데이트
+        this.status = newStatus;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
