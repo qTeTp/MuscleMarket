9aeac5feee17644ed471b6f09918b01f8a172b41
InWoong <dlsdnd122@naver.com>
2025-11-05 15:49:48 +0900

    [FEAT] : 로그아웃 구현

--- muscle_market/src/main/java/com/example/muscle_market/controller/api/AuthController.java
+++ muscle_market/src/main/java/com/example/muscle_market/controller/api/AuthController.java
@@ -5,6 +5,8 @@ import com.example.muscle_market.dto.LoginResponseDto;
 import com.example.muscle_market.dto.UserDto;
 import com.example.muscle_market.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,6 +24,12 @@ public class AuthController {
 
     @PostMapping("/login")
     public LoginResponseDto login(@RequestBody LoginDto loginDto){
-        return userService.login(loginDto); // JWT 반환
+        return userService.login(loginDto); // JWT 토큰들 반환 (access token, refresh token)
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@AuthenticationPrincipal String username){
+        userService.logout();
+        return ResponseEntity.ok("로그아웃 완료");
     }
 }
