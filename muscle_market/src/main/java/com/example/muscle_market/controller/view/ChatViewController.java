128e0190b29fdbdcb283a54fca2e0c45191a9540
Seokwon You <youseokwon1667@gmail.com>
2025-11-05 14:05:08 +0000

    [FIX] 채팅 에러 해결 - 메시지 엔티티의 id 변수 이름이 잘못되어 있었음 - jwt 토큰을 위한 소켓 컨트롤러 세팅 수정 - TODO: 자바스크립트에서 jwt 관련 설정 필요

--- muscle_market/src/main/java/com/example/muscle_market/controller/view/ChatViewController.java
+++ muscle_market/src/main/java/com/example/muscle_market/controller/view/ChatViewController.java
@@ -1,33 +1,32 @@
-//package com.example.muscle_market.controller.view;
-//
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import com.example.muscle_market.domain.CustomUserDetails;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//import com.example.muscle_market.domain.User;
-//import com.example.muscle_market.dto.ChatUserDto;
-//
-//
-//@Controller
-//@RequiredArgsConstructor
-//@RequestMapping("/chats")
-//public class ChatViewController {
-//    @GetMapping
-//    public String getChatListView(Model model, @AuthenticationPrincipal CustomUserDetails authUser) {
-//        User user = authUser.getUser();
-//        ChatUserDto currentUser = ChatUserDto.builder()
-//            .userId(user.getId())
-//            .nickname(user.getNickname())
-//            .profileImageUrl(user.getProfileImgUrl())
-//            .build();
-//
-//        model.addAttribute("currentUser", currentUser);
-//        return "/chat/chatList";
-//    }
-//
-//}
-//
+package com.example.muscle_market.controller.view;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.muscle_market.domain.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.muscle_market.domain.User;
+import com.example.muscle_market.dto.ChatUserDto;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/chats")
+public class ChatViewController {
+   @GetMapping
+   public String getChatListView(Model model, @AuthenticationPrincipal CustomUserDetails authUser) {
+       ChatUserDto currentUser = ChatUserDto.builder()
+           .userId(authUser.getId())
+           .nickname(authUser.getNickname())
+           .profileImageUrl(authUser.getProfileImgUrl())
+           .build();
+
+       model.addAttribute("currentUser", currentUser);
+       return "/chat/chatList";
+   }
+
+}
+
