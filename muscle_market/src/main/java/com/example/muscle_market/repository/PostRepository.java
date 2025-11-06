95d11ad6cf64a8229cdb363b4a14795524e586f8
Seokwon You <youseokwon1667@gmail.com>
2025-11-06 13:35:45 +0000

    [FEAT] 게시글 CRUD 컨트롤러/서비스 구현 - CRUD 외에도 검색 기능 추가 - 채팅쪽에 물품 게시글 관련 검증 에러 해결

--- muscle_market/src/main/java/com/example/muscle_market/repository/PostRepository.java
+++ muscle_market/src/main/java/com/example/muscle_market/repository/PostRepository.java
@@ -1,11 +1,24 @@
 package com.example.muscle_market.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.muscle_market.domain.Post;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+    // id로 탐색할 때 포스트 이미지들도 로드하는 경우 사용
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.postImages WHERE p.postId = :postId")
+    Optional<Post> findByIdWithImages(@Param("postId") Long postId);
 
+    // 게시글 조회수 증가
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Post p SET p.views = COALESCE(p.views, 0) + 1 WHERE p.id = :postId")
+    void increaseView(@Param("postId") Long postId);
 }
