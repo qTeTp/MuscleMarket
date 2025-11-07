6c9861206c00719a0fbb58adc05d8d63cb603b4a
Seokwon You <youseokwon1667@gmail.com>
2025-11-07 09:10:38 +0000

    [FIX] 번개 엔티티 게시글 엔티티로 통합 - 별도의 테이블을 두고 게시글에 필드 내용을 content로 적는 것은 비효율적이라 통합 - 그에 따라 DTO, Service, Controller 수정 - 게시글 숨김/숨김 해제 추가 - 전역 예외 핸들러 클래스 생성됨

--- muscle_market/src/main/java/com/example/muscle_market/dto/PostDetailDto.java
+++ muscle_market/src/main/java/com/example/muscle_market/dto/PostDetailDto.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import com.example.muscle_market.domain.Post;
 import com.example.muscle_market.domain.PostImage;
+import com.example.muscle_market.enums.BungaeStatus;
+import com.example.muscle_market.enums.PostStatus;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -16,21 +18,26 @@ public class PostDetailDto {
     private String title;
     private String content;
     private String sportName;
-    private Long bungaeId;
     private LocalDateTime createdAt;
+    private Integer views;
+    private PostStatus postStatus;
     private PostUserDto postAuthor;
+    private Boolean isBungae;
+    private String bungaeLocation;
+    private Integer maxParticipants;
+    private Integer curParticipants;
+    private String bungaeDatetime;
+    private BungaeStatus bungaeStatus;
     private List<String> postImages;
 
-    public static PostDetailDto fromEntity(Post post) {
-        Long bungaeId = post.getBungae() == null ? null : post.getBungae().getBungaeId();
-        
+    public static PostDetailDto fromEntity(Post post) {        
         return PostDetailDto.builder()
             .postId(post.getPostId())
             .title(post.getTitle())
             .content(post.getContent())
             .sportName(post.getSport().getName())
-            .bungaeId(bungaeId)
             .createdAt(post.getCreatedAt())
+            .views(post.getViews())
             .postAuthor(PostUserDto.builder()
                 .authorId(post.getAuthor().getId())
                 .authorUsername(post.getAuthor().getUsername())
@@ -38,6 +45,12 @@ public class PostDetailDto {
                 .authorProfileImgUrl(post.getAuthor().getProfileImgUrl())
                 .build()
             )
+            .isBungae(post.getIsBungae())
+            .bungaeLocation(post.getBungaeLocation())
+            .maxParticipants(post.getMaxParticipants())
+            .curParticipants(post.getCurParticipants())
+            .bungaeDatetime(post.getBungaeDatetime())
+            .bungaeStatus(post.getBungaeStatus())
             .postImages(post.getPostImages().stream().map(PostImage::getImageUrl).toList())
             .build();
     }
