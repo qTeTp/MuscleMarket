cf4ae6f2c1f3cf0eecf83c3f906f4d178748ac3c
qTeTp <68058317+qTeTp@users.noreply.github.com>
2025-11-18 16:04:07 +0900

    [FEAT] 사용자 온보딩 수정 프포필 버튼 드롭다운 통해 접근

--- muscle_market/src/main/java/com/example/muscle_market/controller/api/OnboardingController.java
+++ muscle_market/src/main/java/com/example/muscle_market/controller/api/OnboardingController.java
@@ -2,6 +2,7 @@ package com.example.muscle_market.controller.api;
 
 import com.example.muscle_market.domain.CustomUserDetails;
 import com.example.muscle_market.dto.OnboardingDto;
+import com.example.muscle_market.dto.UserFavoriteSportDto;
 import com.example.muscle_market.service.OnboardingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,6 @@ import jakarta.validation.Valid;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class OnboardingController {
-
     private final OnboardingService onboardingService;
 
     // 온보딩 입력
@@ -22,7 +22,6 @@ public class OnboardingController {
             @Valid @RequestBody OnboardingDto onboardingDto) {
         String msg = onboardingService.completeOnboarding(onboardingDto);
         return ResponseEntity.ok(msg);
-
     }
 
     // 온보딩 수정
@@ -33,4 +32,16 @@ public class OnboardingController {
         return ResponseEntity.ok(msg);
     }
 
+    // 유저id를 통해 사용자의 선호 운동 반환
+    @GetMapping("/favorite-sport")
+    public ResponseEntity<UserFavoriteSportDto> getOnboarding(
+            @AuthenticationPrincipal CustomUserDetails principal) {
+        Long userId = principal.getId();
+        try {
+            UserFavoriteSportDto dto = onboardingService.getFavoriteSport();
+            return ResponseEntity.ok(dto);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
 }
