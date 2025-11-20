a40bececea08c595422ee2d3613ed7e619d0f3ca
InWoong <dlsdnd122@naver.com>
2025-11-20 10:09:37 +0900

    [REFACTOR] : homeContorller 로그인되어있을 경우 products 페이지로 리다이렉트 되도록 수정

--- muscle_market/src/main/java/com/example/muscle_market/controller/view/HomeController.java
+++ muscle_market/src/main/java/com/example/muscle_market/controller/view/HomeController.java
@@ -1,12 +1,23 @@
 package com.example.muscle_market.controller.view;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
     public String root() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 로그인된 상태면 poducts 페이지로 리다이렉트
+        if (authentication != null && authentication.isAuthenticated()
+                && !"anonymousUser".equals(authentication.getPrincipal())) {
+            return "redirect:/products";
+        }
+
         return "redirect:/login";
     }
 }
